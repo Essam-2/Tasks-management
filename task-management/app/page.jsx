750900c3ade470590955830e079f9709507072fb
Essam <esamanwar2@gmail.com>
2024-02-15 00:26:53 +0300

    [+] add new Home page with the name Dashboard

---
+++ task-management/app/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TaskCard from "./{components}/TaskCard";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <TaskCard />
+      <TaskCard />
+      <TaskCard />
+    </div>
+  );
+};
+
+export default Dashboard;
