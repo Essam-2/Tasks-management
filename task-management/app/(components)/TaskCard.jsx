4fefae888e9fcc14c7e20d811f5fd4b57e064245
Essam <esamanwar2@gmail.com>
2024-02-15 06:13:19 +0300

    [+] pass the data that comes from the DB

--- task-management/app/(components)/TaskCard.jsx
+++ task-management/app/(components)/TaskCard.jsx
@@ -4,29 +4,44 @@ import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
-const TaskCard = () => {
+const TaskCard = ({ id, key, task }) => {
+  function formatTimestamp(timestamp) {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleString("en-US", options);
+
+    return formattedDate;
+  }
+
+  const createdDateTime = formatTimestamp(task.createdAt);
+
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2 shadow-gray-800">
       <div className="flex mb-3 ">
-        <PriorityDisplay />
+        <PriorityDisplay priority={task.priority} />
         <div className="ml-auto">
           <DeleteBlock />
         </div>
       </div>
-      <h4>Task Title</h4>
+      <h4>{task.title}</h4>
       <hr className=" h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap">
-        {" "}
-        This is the task description Please do it{" "}
-      </p>
+      <p className="whitespace-pre-wrap">{task.description}</p>
       <div className="flex-grow"></div>
       <div className="flex mt-2 ">
         <div className="flex flex-col">
-          <p className="text-xs my-1">15/2/2024 12:22 PM</p>
-          <ProgressDisplay />
+          <p className="text-xs my-1">{createdDateTime}</p>
+          <ProgressDisplay progress={task.progress} />
         </div>
         <div className="ml-auto flex items-end">
-          <StatusDisplay />
+          <StatusDisplay status={task.status} />
         </div>
       </div>
     </div>
