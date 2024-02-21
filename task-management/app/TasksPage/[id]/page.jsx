808b2f93209961b367a0dc2c73b74189324c7b17
Essam <esamanwar2@gmail.com>
2024-02-21 04:09:33 +0300

    [+] get task by id

--- task-management/app/TasksPage/[id]/page.jsx
+++ task-management/app/TasksPage/[id]/page.jsx
@@ -1,8 +1,35 @@
 import TaskForm from "@/app/(components)/TaskForm";
 import React from "react";
 
-const TasksPage = ({ params }) => {
-  return <TaskForm />;
+const getTaskById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/Tasks/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topic");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const TasksPage = async ({ params }) => {
+  const EDITMODE = params.id === "new" ? false : true;
+  let updateTaskData = {};
+
+  if (EDITMODE) {
+    updateTaskData = await getTaskById(params.id);
+    updateTaskData = updateTaskData.foundTask;
+  } else {
+    updateTaskData = {
+      _id: "new",
+    };
+  }
+  return <TaskForm task={updateTaskData} />;
 };
 
 export default TasksPage;
