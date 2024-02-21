6a36c50e6e3e0134082fdba651e0f108f72ace69
Essam <esamanwar2@gmail.com>
2024-02-21 04:07:52 +0300

    [+] Delete functionally

--- task-management/app/(components)/DeleteBlock.jsx
+++ task-management/app/(components)/DeleteBlock.jsx
@@ -1,12 +1,26 @@
+"use client";
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const DeleteBlock = () => {
+  const router = useRouter();
+
+  const deleteTask = async ({ id }) => {
+    const res = await fetch(`http://localhost:3000/api/Tasks/${id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      router.refresh();
+    }
+  };
+
   return (
     <FontAwesomeIcon
       icon={faX}
       className="text-red-400 hover:cursor-pointer hover:text-red-200"
+      onClick={deleteTask}
     />
   );
 };
